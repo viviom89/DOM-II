48f9b996d3a712f36453cfe86f6553d9459a6a4a
Chhorvy Om <c_om89@live.com>
2022-07-31 11:38:19 -0700

    Added Event Listener for mousemove, mouseenter, and mouseleave

--- src/index.js
+++ src/index.js
@@ -26,4 +26,21 @@ window.addEventListener("keydown", evt => {
         document.body.innerHTML = "<h1>HELLO!!!</h1>"
     }
 })
+
+document.body.addEventListener("mousemove", evt => {
+    const { clientX, clientY } = evt
+})
+
+const destinations = document.querySelectorAll(".destination")
+for (let destination of destinations) {
+    destination.addEventListener("mouseenter", () => {
+        destination.style.fontWeight = "bold"
+    })
+    destination.addEventListener("mouseleave", () => {
+        setTimeout(() => {
+            destination.style.fontWeight = "initial"
+        }, 500)
+        })
+    }
+
 }

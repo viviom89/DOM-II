4a9d5453a4d129fcadc23de4d57140cd0144e6a5
Chhorvy Om <c_om89@live.com>
2022-07-31 10:45:01 -0700

    Added Event Listener for click, dblclick, and keydown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './less/index.less'
 
 // Your code goes here!
+window.onload = function (evt) {
+    console.log(`event ${evt.type} Start up!`)
+    const heading = document.querySelector("h1")
+    heading.textContent = "Fun Begins Here!!!"
+
+window.addEventListener("copy", () => {
+    navigator.clipboard.readText()
+    .then(text => {
+        heading.textContent += text
+    })
+})
+
+document.body.addEventListener("click", evt => {
+    evt.target.classList.toglle("mirror")
+})
+
+document.body.addEventListener("dblclick", evt => {
+    evt.target.innerHTML = " "
+})
+
+window.addEventListener("keydown", evt => {
+    if (evt.key == 6) {
+        document.body.innerHTML = "<h1>HELLO!!!</h1>"
+    }
+})
+}
